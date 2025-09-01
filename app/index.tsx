e531a49043a5b42be0f8d2f2113d754b9d6d152a
hinodi <huynhngocdinh98@gmail.com>
2025-09-01 13:45:46 +0700

    issue

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { Image as ExpoImage } from 'expo-image';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, View } from 'react-native';
 
-export default function Index() {
+export default function ImagePickerExample() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({});
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Button title="Pick an image" onPress={pickImage} />
+      {!!image && <Image source={{ uri: image }} style={styles.image} />}
+      {!!image && <ExpoImage source={{ uri: image }} style={styles.image} />}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+});
